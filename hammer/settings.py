254eba2938ed5e7bf723d3dcad8d8b19ffe87254
Mark Seaman <Mark.Seaman@imac.net>
2019-07-01 20:08:03 -0600

    Todo list

--- hammer/settings.py
+++ hammer/settings.py
@@ -88,9 +88,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
+TIME_ZONE = 'US/Mountain'
+USE_I18N = False
+USE_L10N = False
 USE_TZ = True
 
 
