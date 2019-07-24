a4af0237b614fca63b1197e4fd5fa758108de225
Mark Seaman <markseaman@MacBook>
2019-07-24 14:35:48 -0600

    Remove hammer db

--- test/selenium_test.py
+++ test/selenium_test.py
@@ -1,9 +1,9 @@
-from tool.shell import is_server
+from tool.shell import is_server, is_macbook
 from tool.page import verify_page
 
 
 def selenium_features_test():
-    if not is_server():
+    if not is_server() and not is_macbook():
         return verify_page('http://localhost:8000/MarkSeaman')
     else:
         return 'Test is disabled on the Sensei-Server'
