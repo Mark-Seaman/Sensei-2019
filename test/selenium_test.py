5d091a41fcfb4a8464dcfcc4e10d552af00467b8
Mark Seaman <Mark.Seaman@imac.net>
2019-07-29 13:20:47 -0600

    Selenium test of all domains

--- test/selenium_test.py
+++ test/selenium_test.py
@@ -3,13 +3,13 @@ from tool.page import end_browser, start_browser, page_features
 
 def selenium_features_test():
     driver = start_browser()
-    features = page_features(driver, 'https://shrinking-world.com')
-    features += page_features(driver, 'https://MarkSeaman.org')
-    features += page_features(driver, 'https://SeamansLog.com')
-    features += page_features(driver, 'https://Spiritual-Things.org')
-    features += page_features(driver, 'http://unco-bacs.org/bacs200/class/templates/simple.html')
+    features = []
+    domains = ['https://MarkSeaman.org', 'https://shrinking-world.com', 'https://SeamansLog.com',
+               'https://Spiritual-Things.org', 'http://unco-bacs.org/bacs200/class/templates/simple.html']
+    for domain in domains:
+        features.append(page_features(driver, domain))
     end_browser(driver)
-    return features
+    return '\n\n'.join(features)
 
 
 if __name__ == '__main__':
