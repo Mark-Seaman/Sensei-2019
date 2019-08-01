e20bc27f7f95982cce1cd8bbf8e83557a8de5e1c
Mark Seaman <Mark.Seaman@imac.net>
2019-08-01 13:33:29 -0600

    Build UNC page test

--- test/unc_test.py
+++ test/unc_test.py
@@ -1,5 +1,23 @@
+from tool.page import close_browser_dom, open_browser_dom, check_page_features, requirements_summary
+from tool.shell import is_server
+
+
 def unc_views_test():
-    return 'UNC VIEWS - build test'
+
+    if is_server():
+        return 'No Selenium on Sensei Server'
+    else:
+        page = open_browser_dom()
+
+        # url = 'http://unco-bacs.org/bacs200/class/templates/simple.html'
+        url = 'https://shrinking-world.com/homework/'
+        requirements = ['head', 'body', 'h1', 'title']
+        page.get(url)
+        features = check_page_features(page, requirements)
+        summary = requirements_summary(features)
+
+        close_browser_dom(page)
+        return summary
 
 
 def unc_data_test():
