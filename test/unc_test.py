fc9211cd7ce6aee304066672a0893e0910f59e9f
Mark Seaman <Mark.Seaman@imac.net>
2019-08-15 13:13:49 -0600

    Refactor page tests

--- test/unc_test.py
+++ test/unc_test.py
@@ -3,19 +3,20 @@ from tool.shell import banner, is_server, text_join
 from unc.bacs import build_projects, initialize_data, print_data, print_projects, zybooks_link
 
 
-def unc_views_test():
+def validate_unc_project(dom, course, project, ):
+    return banner('PROJECT %s' % project) + display_test_results(validate_project_page(dom, course, project))
 
+
+def unc_views_test():
     if is_server():
         return 'No Selenium on Sensei Server'
     else:
-        page = open_browser_dom()
-        summary = banner('PROJECT 01') + display_test_results(validate_project_page('bacs200', '01'))
-        summary += banner('PROJECT 02') + display_test_results(validate_project_page('bacs200', '02'))
-        summary += banner('PROJECT 03') + display_test_results(validate_project_page('bacs200', '03'))
-        close_browser_dom(page)
+        dom = open_browser_dom()
+        summary = validate_unc_project(dom, 'bacs200', '01')
+        summary += validate_unc_project(dom, 'bacs200', '02')
+        summary += validate_unc_project(dom, 'bacs200', '03')
+        close_browser_dom(dom)
         return summary
-
-    # print('eval: %s' % eval('transform_output("four score")'))
 
 
 def unc_data_test():
