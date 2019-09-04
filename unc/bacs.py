57f76799b4ccb9ab071aacb5df059abe3bf4e61d
Mark Seaman <Mark.Seaman@imac.net>
2019-09-04 08:07:31 -0600

    Week 2

--- unc/bacs.py
+++ unc/bacs.py
@@ -194,7 +194,7 @@ def weekly_agenda(course, week):
 
 
 def weekly_lessons(course):
-    return [weekly_agenda(course, week + 1) for week in range(1)]
+    return [weekly_agenda(course, week + 1) for week in range(2)]
 
 
 def zybooks_link(course, reading):
