19770de8b65adaea4010e70310d9545248363532
Mark Seaman <Mark.Seaman@imac.net>
2019-09-15 11:55:47 -0600

    350-skill-3

--- unc/skills.py
+++ unc/skills.py
@@ -6,6 +6,7 @@ def build_skills():
     Skill.create('bacs200', 2, 'FTP',           '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
     Skill.create('bacs350', 1, 'Web Hosting',   '2019-09-16', 'bluehost.png,wordpress.png')
     Skill.create('bacs350', 2, 'FTP',           '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
+    Skill.create('bacs350', 3, 'Github',        '2019-09-16', 'git-files.png,git-changes.png')
 
 
 def print_skills(course):
