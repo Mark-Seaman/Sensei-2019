f7c71ec8a8774ff8abb22d1eeb5339983ea02a36
Mark Seaman <Mark.Seaman@imac.net>
2019-10-09 11:28:00 -0600

    lesson 10-09

--- unc/skills.py
+++ unc/skills.py
@@ -1,28 +1,25 @@
 from unc.models import Skill
 
 
-def build_skills():
-    Skill.create('bacs200', 1, 'Web Hosting',       '2019-09-16', 'bluehost.png,wordpress.png')
-    Skill.create('bacs200', 2, 'FTP',               '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
-    Skill.create('bacs200', 3, 'Brackets',          '2019-09-20', 'brackets.png')
-    Skill.create('bacs200', 4, 'URL Game',          '2019-09-23', 'urlgame.png')
-    Skill.create('bacs200', 5, 'Validate HTML',     '2019-09-30', 'validator-ok.png,validator-link.png')
-    Skill.create('bacs200', 6, 'Developer Tools',   '2019-10-04', 'devtools.png')
-    Skill.create('bacs200', 7, 'New Page Template', '2019-10-09', 'newpage.png')
-
-    Skill.create('bacs350', 1, 'Web Hosting',   '2019-09-16', 'bluehost.png,wordpress.png')
-    Skill.create('bacs350', 2, 'FTP',           '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
-    Skill.create('bacs350', 3, 'Github',        '2019-09-16', 'git-files.png,git-changes.png')
-    Skill.create('bacs350', 4, 'Share Code',    '2019-09-20', 'git-pull.png')
-    Skill.create('bacs350', 5, 'Apache',        '2019-09-25', 'apache.png')
-    Skill.create('bacs350', 6, 'Database',      '2019-10-02', 'database.png')
-    Skill.create('bacs350', 7, 'Dev Workflow',  '2019-10-09', 'workflow.png,crud.png')
-
-
 def print_skills(course):
     return '\n'.join(Skill.list(course))
 
 
 def update_skills():
-    build_skills()
+    Skill.create('bacs200', 1, 'Web Hosting', '2019-09-16', 'bluehost.png,wordpress.png')
+    Skill.create('bacs200', 2, 'FTP', '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
+    Skill.create('bacs200', 3, 'Brackets', '2019-09-20', 'brackets.png')
+    Skill.create('bacs200', 4, 'URL Game', '2019-09-23', 'urlgame.png')
+    Skill.create('bacs200', 5, 'Validate HTML', '2019-09-30', 'validator-ok.png,validator-link.png')
+    Skill.create('bacs200', 6, 'Developer Tools', '2019-10-04', 'devtools.png')
+    Skill.create('bacs200', 7, 'New Page Template', '2019-10-09', 'newpage.png,article.png')
+
+    Skill.create('bacs350', 1, 'Web Hosting', '2019-09-16', 'bluehost.png,wordpress.png')
+    Skill.create('bacs350', 2, 'FTP', '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
+    Skill.create('bacs350', 3, 'Github', '2019-09-16', 'git-files.png,git-changes.png')
+    Skill.create('bacs350', 4, 'Share Code', '2019-09-20', 'git-pull.png')
+    Skill.create('bacs350', 5, 'Apache', '2019-09-25', 'apache.png')
+    Skill.create('bacs350', 6, 'Database', '2019-10-02', 'database.png')
+    Skill.create('bacs350', 7, 'Dev Workflow', '2019-10-09', 'workflow.png,crud.png')
+
     return print_skills('bacs350') + '\n' + print_skills('bacs200')
