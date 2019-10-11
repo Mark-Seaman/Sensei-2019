f582b6682ee24b2e6529173e962771ead333ae16
Mark Seaman <Mark.Seaman@imac.net>
2019-10-11 10:01:10 -0600

    prep 10-14

--- unc/skills.py
+++ unc/skills.py
@@ -15,7 +15,6 @@ def update_skills():
     Skill.create('bacs200', 7, 'New Page Template', '2019-10-09', 'newpage.png,article.png')
     Skill.create('bacs200', 8, 'Using Bootstrap', '2019-10-11', 'bootstrap.png')
 
-
     Skill.create('bacs350', 1, 'Web Hosting', '2019-09-16', 'bluehost.png,wordpress.png')
     Skill.create('bacs350', 2, 'FTP', '2019-09-16', 'ftp-site-manager.png,ftp-dirs.png,ftp-files.png')
     Skill.create('bacs350', 3, 'Github', '2019-09-16', 'git-files.png,git-changes.png')
@@ -23,5 +22,6 @@ def update_skills():
     Skill.create('bacs350', 5, 'Apache', '2019-09-25', 'apache.png')
     Skill.create('bacs350', 6, 'Database', '2019-10-02', 'database.png')
     Skill.create('bacs350', 7, 'Dev Workflow', '2019-10-09', 'workflow.png,crud.png')
+    Skill.create('bacs350', 8, 'Data Views', '2019-10-16', 'list.png,add.png,detail.png,edit.png')
 
     return print_skills('bacs350') + '\n' + print_skills('bacs200')
