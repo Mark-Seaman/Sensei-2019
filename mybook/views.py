1cd78a4e29e2c44b9087b5e3a2669cbce0544ee9
Mark Seaman <Mark.Seaman@imac.net>
2019-10-24 13:14:50 -0600

    deploy Security Violation

--- mybook/views.py
+++ mybook/views.py
@@ -50,8 +50,9 @@ class DocDisplay(TemplateView):
         title = self.kwargs.get('title', 'Index')
         if title.startswith('info'):
             if not self.request.user.is_superuser:
-                log('SECURITY VIOLATION: %s --> %s' % (title, '/unc/logout'))
-                return HttpResponseRedirect('/unc/logout')
+                redir = 'https://shrinking-world.com/shrinkingworld/SecurityViolation'
+                log('SECURITY VIOLATION: %s --> %s' % (title, redir))
+                return HttpResponseRedirect(redir)
         url = doc_page(self.request.path[1:])
         if url:
             log('REDIRECT: %s --> %s' % (title, url))
