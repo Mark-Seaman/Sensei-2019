06de0868c7b58f5d47ece16fcfb071954f8f8375
Mark Seaman <Mark.Seaman@imac.net>
2019-12-15 14:01:39 -0700

    Update insight.py

--- insight/insight.py
+++ insight/insight.py
@@ -56,10 +56,10 @@ def monthly_insights_panel(year, month, active):
 
 def monthly_insights():
     monthly = [
-        monthly_insights_panel(2019, 9,  False),
+        monthly_insights_panel(2019, 9,  True),
         monthly_insights_panel(2019, 10, False),
         monthly_insights_panel(2019, 11, False),
-        monthly_insights_panel(2019, 12, True),
+        monthly_insights_panel(2019, 12, False),
     ]
     return dict(groups=monthly)
 
