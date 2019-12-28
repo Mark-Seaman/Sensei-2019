e20373fbdc426a4e4aa293ff1728036092af6cf4
Mark Seaman <markseaman@MacBook>
2019-12-28 05:41:23 -0700

    Time and HTML course outline

---
+++ tool/outline.py
@@ -0,0 +1,63 @@
+from os.path import join
+
+# from bin.pandoc import text_to_html
+
+
+def has_kids(text, depth):
+    return len(split_text(text, seperator(depth+1)))>1
+
+
+def outline(text):
+    tree = parse_outline(text, [], 1)
+    # print(tree)
+    return tree
+
+
+def parse_outline(text, tree, depth):
+    kids = []
+    if has_kids(text, depth):
+        for subtext in split_text(text, seperator(depth+1))[1:]:
+            # print_node(title(subtext, depth+1)[0], depth+1)
+            kids = parse_outline(subtext, kids, depth+1)
+    node = title(text, depth)
+    if kids:
+        tree.append((node[0], kids, node[1]))
+    else:
+        tree.append((node[0], None, node[1]))
+    return tree
+
+
+def print_node(name, depth=1, char='    '):
+    print('%s %s' % (char * depth, name))
+
+
+def seperator(depth):
+    return '\n%s ' % ("#"*depth)
+
+
+def split_text(text, sep):
+    return [x for x in text.split(sep) if x]
+
+
+def title(text, depth):
+    parts = split_text(text, seperator(depth+1))
+    text = parts[0]
+    x = len(text.split('\n')[0])
+    title = text[:x]
+    body = str(text[x + 1:])
+    return title, body
+
+
+# def read_cards(doc):
+#     text = open(join('Documents', doc)).read()[2:]
+#     sections = split_text(text, seperator(2))
+#     return [title(x, 3) for x in sections]
+#
+#
+# def tabs_data(doc):
+#
+#     def tab_choice(i,tab):
+#         return ('tab%d'%i, tab[0], tab[1], 'active' if i==0 else '')
+#
+#     tabs = read_cards(doc)
+#     return [tab_choice(i,tab) for i,tab in enumerate(tabs)]
--- tool/quick_test.py
+++ tool/quick_test.py
@@ -1,50 +1,46 @@
-from insight.insight import *
 from insight.models import *
 from tool.days import *
-from unc.bacs import *
-from unc.projects import *
-from unc.models import *
+from tool.text import *
 from unc.review import *
 
 
 def quick_test():
-    for day in enumerate_month(2019, 4):
+    text = text_lines(open('Documents/info/Test/Outline3').read())
+    text = [line for line in text if line.strip()]
+    tree, text = outline(text)
+    print_outline_indent(tree)
+
+
+def outline(text, indent=''):
+    tree = []
+    while text and text[0].startswith(indent):
+        label = text[0].strip()
+        text = text[1:]
+        if label:
+            node = [label]
+            # print(node[0], '%d' % len(indent))
+            kids, text = outline(text, ' '*4+indent)
+            if kids:
+                node += kids
+            tree.append(node)
+    return tree, text
+
+
+def print_outline_markdown(tree, indent='#'):
+    for node in tree:
+        print(indent + " " + node[0])
+        print_outline_markdown(node[1:], indent+'#')
+
+
+def print_outline_indent(tree, indent=''):
+    for node in tree:
+        print(indent + node[0])
+        print_outline_indent(node[1:], indent+'    ')
+
+
+def create_insights(month):
+    for day in enumerate_month(2019, month):
         date = datetime.strptime(day, "%Y-%m-%d")
         i = Insight.objects.get_or_create(date=date)[0]
         i.save()
         print(i)
-
-
-def show_course_content():
-    x = list_course_content()
-    print(x)
-
-
-def init_unc_data():
-    x = list_course_content()
-    print(x)
-
-    # import_skills('bacs200')
-    # import_skills('bacs350')
-
-    # import_lessons('bacs200')
-    # import_lessons('bacs350')
-
-    # import_projects('bacs350')
-    # import_projects('bacs200')
-
-    # export_skills('bacs200')
-    # export_skills('bacs350')
-
-    # export_lessons('bacs200')
-    # export_lessons('bacs350')
-
-    # export_projects('bacs350')
-    # export_projects('bacs200')
-
-    # Lesson.objects.filter(lesson=-1).delete()
-    # init_unc_data()
-    # print(print_projects())
-
-
-
